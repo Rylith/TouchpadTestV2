59a4fb3434b3e8d7d73a8303ea7f9501f41b8f4b
Vivien-Michel <vivien.michel@e.ujf-grenoble.fr>
2016-07-13 13:00:20 +0200

    activation border mode en mettant une marge de 5%

--- MouseMotionServer/src/mouse/control/MouseListener.java
+++ MouseMotionServer/src/mouse/control/MouseListener.java
@@ -16,7 +16,7 @@ public class MouseListener {
 	private Point prec;
 	
 	private int RAYON;
-	private static final float PERCENTSCREENSIZE = 0.00f;
+	private static final float PERCENTSCREENSIZE = 0.05f;
 	
 	private static double MARGE = 40;
     private double[] bufferX = new double[20];
